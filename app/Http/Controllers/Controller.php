15220b5428abd1234399798c6221076f312aa37e
yutaro.furuta <furutaro123@gmail.com>
2019-05-31 08:39:54 +0000

    post

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function count($user) {
+        $count_microposts = $user->microposts()->count();
+        
+        return  [
+            'count_microposts' => $count_microposts,
+        ]
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -25,4 +23,5 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ユーザ機能
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('posts', 'MicropostsController', ['only' => ['create', 'destroy']]);
 });
